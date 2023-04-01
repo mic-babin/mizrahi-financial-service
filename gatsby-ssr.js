8c7f8dbb823ed487056f8531c1573bda0a838353
Michael Babin <mic.babin@outlook.com>
2023-04-01 08:02:43 -0400

    remove fonts preload

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,30 +2,4 @@ import * as React from "react";
 
 export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: "en" });
-  setHeadComponents([
-    <link
-      rel="preload"
-      href="/fonts/SF-Light.ttf"
-      as="font"
-      type="font/ttf"
-      crossOrigin="anonymous"
-      key="SF-Display-Light"
-    />,
-    <link
-      rel="preload"
-      href="/fonts/SF-Regular.ttf"
-      as="font"
-      type="font/ttf"
-      crossOrigin="anonymous"
-      key="SF-Display-Regular"
-    />,
-    <link
-      rel="preload"
-      href="/fonts/TimeNow-Light.ttf"
-      as="font"
-      type="font/ttf"
-      crossOrigin="anonymous"
-      key="TimesNow"
-    />,
-  ]);
 };
